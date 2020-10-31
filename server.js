12eff2b8df47b1bfab307ca6131a1830470ac3a1
SendyMederos <sendymederos@hotmail.com>
2020-10-31 08:59:09 -0400

    trying

--- server.js
+++ server.js
@@ -7,10 +7,21 @@
 var express = require("express");
 // Sets up the Express App
 // =============================================================
+var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 8080;
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log(`App listening on PORT` + PORT)
+  });
+});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
 
-var db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -25,8 +36,8 @@ require("./routes/members-api-routes.js")(app);
 require("./routes/sessions-api-routes.js")(app);
 require("./routes/session-memeber-api-routes.js")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
+db.sequelize.sync({ force: true }).then(function () {
+  app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
   });
 });
